a8c0546252981229d91363ed1b08d7ddadb00d3f
Francisco de Assis <a124fr@gmail.com>
2022-06-25 19:00:39 -0300

    Implementação SEM Redux

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    const {min, max} = props;
     return (
         <Card title='Médio dos Números' green>
             <div>
                 <span>
                     <strong>Resultado: </strong>
-                    <strong>5</strong>
+                    <strong>{(max + min) / 2}</strong>
                 </span>                
             </div>
         </Card>
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    const {min, max} = props;
     return (
         <Card title='Soma dos Números' blue>
             <div>
                 <span>
                     <strong>Resultado: </strong>
-                    <strong>10</strong>
+                    <strong>{max + min}</strong>
                 </span>                
             </div>
         </Card>
