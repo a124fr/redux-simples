f3fbeafd1b27cd0a5db4a7f187f7e7a49d643540
Francisco de Assis <a124fr@gmail.com>
2022-06-25 19:29:02 -0300

    Configurando o redux na aplicação

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux'
+import storeConfig from './store/storeConfig'
+
+const store = storeConfig();
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
