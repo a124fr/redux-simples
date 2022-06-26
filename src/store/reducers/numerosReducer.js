8dad5cd11935f7eb5171efc914e00ff50f241104
Francisco de Assis <a124fr@gmail.com>
2022-06-25 22:54:10 -0300

    refatorando reducer

---
+++ src/store/reducers/numerosReducer.js
@@ -0,0 +1,21 @@
+const initialstate = {
+    min: 1,
+    max: 10
+}
+
+export default function(state = initialstate, action) {
+    switch(action.type) {
+        case 'NUM_MIN_ALTERADO':
+            return {
+                ...state,
+                min: action.payload
+            }
+        case 'NUM_MAX_ALTERADO':
+            return {
+                ...state,
+                max: action.payload
+            }
+        default:
+            return state;
+    }        
+}
