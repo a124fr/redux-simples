0682e996139189e8884cfda1eeb0272c1f2246bc
Francisco de Assis <a124fr@gmail.com>
2022-06-25 23:01:00 -0300

    Constantes para os tipos das Actions

--- src/store/reducers/numerosReducer.js
+++ src/store/reducers/numerosReducer.js
@@ -1,3 +1,5 @@
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from "../actions/actionTypes";
+
 const initialstate = {
     min: 1,
     max: 10
@@ -5,12 +7,12 @@ const initialstate = {
 
 export default function(state = initialstate, action) {
     switch(action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return {
                 ...state,
                 max: action.payload
